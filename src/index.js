e6e4aca1e5be2c3a3649582b93248e335ee0bb68
swagasoft <swagasoft@gmail.com>
2022-02-06 22:06:08 +0100

    middleware implementation

--- src/index.js
+++ src/index.js
@@ -9,23 +9,15 @@ import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-store.subscribe(()=> {
-    console.log('Store changed!')
+// store.subscribe(()=> {
+//     console.log('Store changed!')
+// })
+
+store.dispatch((dispatch, getState)=> {
+//  call external api
+dispatch({type:'error', payload:{message:' error getting data'}})
 })
 
-store.dispatch(projectAdded({name:" project 1"}))
-store.dispatch(bugAdded({description: "Bug 1"}));
-store.dispatch(bugAdded({description:"Bug 2"}));
-store.dispatch(bugAdded({description:"Bug 3"}));
-store.dispatch(userAdded({name:"Simon Jerry"}))
-store.dispatch(bugAssignToUser({userId: 1, bugId: 1}))
-store.dispatch(bugResolved({id:1}))
-
-
-console.log(store.getState())
-
-const bugs = getBugsByUser(1)(store.getState())
-console.log(bugs)
 
 
   
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,9 +2,17 @@
 // import reducer from "./bug";
 import {reducer} from "./reducer";
 // import { devToolsEnhancer } from "redux-devtools-extension";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "./middleware/logger";
+import func from "./middleware/func";
+import toast from "./middleware/toast";
 
 
 export default function (){
-    return configureStore({reducer});
+    return configureStore({
+        reducer,
+        ...getDefaultMiddleware,
+        middleware:[toast, logger("login console"),func,  ]
+        
+    });
 }
